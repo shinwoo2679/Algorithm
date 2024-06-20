1a5a16e4db628d5380dd712c3873b0248e6711b1
shinwoo2679 <144502858+shinwoo2679@users.noreply.github.com>
2024-06-20 23:40:11 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        String id2 = scn.next();
+        System.out.print(id2 + "??!");
+    }
+}
